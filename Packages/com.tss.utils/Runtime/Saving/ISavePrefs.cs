e513b267762394995b96a3e0ffd137c1c29800f4
Pacelin <Warfacer2020@gmail.com>
2025-04-22 22:44:56 +0300

    feat: SaveSystem

---
+++ Packages/com.tss.utils/Runtime/Saving/ISavePrefs.cs
@@ -0,0 +1,12 @@
+﻿namespace TSS.Utils.Saving
+{
+    public interface ISavePrefs
+    {
+        void SetString(string key, string data);
+        string GetString(string key, string defaultValue = "");
+        bool HasKey(string key);
+        void DeleteKey(string key);
+        void ClearData();
+        void Save();
+    }
+}
