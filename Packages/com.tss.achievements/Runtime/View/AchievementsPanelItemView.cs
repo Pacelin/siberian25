87ba51f9a8f67f6019d4661ed22c21852ca694ca
Pacelin <Warfacer2020@gmail.com>
2025-04-29 21:11:56 +0300

    feat: Icon Coloring

--- Packages/com.tss.achievements/Runtime/View/AchievementsPanelItemView.cs
+++ Packages/com.tss.achievements/Runtime/View/AchievementsPanelItemView.cs
@@ -37,6 +37,7 @@ namespace TSS.Achievements.View
             _icon.sprite = _config.LockedIcon;
             _captionText.text = _config.LockedCaption;
             _descriptionText.text = _config.LockedDescription;
+            _icon.color = _config.LockedIconColor;
             _onLock.Invoke();
         }
 
@@ -45,6 +46,7 @@ namespace TSS.Achievements.View
             _icon.sprite = _config.Icon;
             _captionText.text = _config.Caption;
             _descriptionText.text = _config.Description;
+            _icon.color = _config.IconColor;
             _onUnlock.Invoke();
         }
 
@@ -53,6 +55,7 @@ namespace TSS.Achievements.View
             _icon.sprite = _config.LockedIcon;
             _captionText.text = _config.LockedCaption;
             _descriptionText.text = _config.LockedDescription;
+            _icon.color = _config.LockedIconColor;
             _onSecretLock.Invoke();
         } 
         
@@ -61,6 +64,7 @@ namespace TSS.Achievements.View
             _icon.sprite = _config.Icon;
             _captionText.text = _config.Caption;
             _descriptionText.text = _config.Description;
+            _icon.color = _config.IconColor;
             _onSecretUnlock.Invoke();
         }
 
