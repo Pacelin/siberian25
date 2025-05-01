1e90420ad5e39c46dca07199651f249ca5a6b42f
Pacelin <Warfacer2020@gmail.com>
2025-05-01 18:47:55 +0300

    feat: Mixpanel Buttons

---
+++ Assets/_Project/Scripts/ButtonHandlers/MixpanelButton.cs
@@ -0,0 +1,13 @@
+﻿using mixpanel;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Siberian25.UI.ButtonHandlers
+{
+    public class MixpanelButton : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField] private string _event;
+        
+        public void OnPointerClick(PointerEventData eventData) => Mixpanel.Track(_event);
+    }
+}
