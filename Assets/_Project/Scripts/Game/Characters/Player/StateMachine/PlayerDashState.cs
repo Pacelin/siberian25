2bcbbdb93091e14f77256314c23e2cc6d8a415d4
Pacelin <Warfacer2020@gmail.com>
2025-05-04 00:22:38 +0300

    feat: Dash to idle

--- Assets/_Project/Scripts/Game/Characters/Player/StateMachine/PlayerDashState.cs
+++ Assets/_Project/Scripts/Game/Characters/Player/StateMachine/PlayerDashState.cs
@@ -22,6 +22,8 @@ namespace Siberian25.Game.Characters
             Composition.DashTrail.emitting = true;
             Composition.SliceTrigger.SliceDirection = dashVector.normalized;
             Composition.SliceTrigger.EnableSlice = true;
+            Animator.SetFloat(PlayerConstants.ANIMATOR_WALK_FLOAT_X, dashVector.x);
+            Animator.SetFloat(PlayerConstants.ANIMATOR_WALK_FLOAT_Y, dashVector.y);
             Animator.SetFloat(PlayerConstants.ANIMATOR_DASH_FLOAT_X, dashVector.x);
             Animator.SetFloat(PlayerConstants.ANIMATOR_DASH_FLOAT_Y, dashVector.y);
             Animator.SetBool(PlayerConstants.ANIMATOR_DASH_HORIZONTAL_BOOL, Mathf.Abs(dashVector.x) > 0.2f);
