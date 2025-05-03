79534ca4fe01038c635beb15f1d4b940be5494ed
Pacelin <Warfacer2020@gmail.com>
2025-05-03 19:13:11 +0300

    feat: Some new

--- Assets/_Project/Scripts/Game/World/GridLayoutFactory.cs
+++ Assets/_Project/Scripts/Game/World/GridLayoutFactory.cs
@@ -31,7 +31,7 @@ namespace Siberian25.Game.World
             }
         }
 
-        private void OnDrawGizmosSelected()
+        private void OnDrawGizmos()
         {
             for (int i = 0; i < transform.childCount; i++)
                 Gizmos.DrawSphere(transform.GetChild(i).position, 0.05f);
