470ae10a130de448a9efba212fbdbbac4d203829
vrusakov <vrusakov@gear-games.com>
2025-05-03 17:06:29 +0300

    bullet sprites

--- Assets/_Project/Scripts/Game/Bullets/BulletTypeData.cs
+++ Assets/_Project/Scripts/Game/Bullets/BulletTypeData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Siberian25.Game.Bullets
 {
@@ -7,5 +8,8 @@ namespace Siberian25.Game.Bullets
 	public class BulletTypeData
 	{
 		public Color startColor;
+        public Sprite sprite;
+        [FormerlySerializedAs("alignWithDirection")]
+        public bool alignToDirection = true;
 	}
 }
