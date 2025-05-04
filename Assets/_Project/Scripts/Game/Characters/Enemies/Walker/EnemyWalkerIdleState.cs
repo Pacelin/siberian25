fa8531cc137ff0cd0ff8f80154e66c79aa7816d9
Pacelin <Warfacer2020@gmail.com>
2025-05-04 14:03:17 +0300

    feat: Walker State

---
+++ Assets/_Project/Scripts/Game/Characters/Enemies/Walker/EnemyWalkerIdleState.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Siberian25.Game.Characters.Enemies
+{
+    public class EnemyWalkerIdleState : EnemyWalkerState
+    {
+        private float _time;
+        private float _duration;
+        
+        public EnemyWalkerIdleState(EnemyComposition composition, EnemyWalkerStateMachine stateMachine) : base(composition, stateMachine)
+        {
+        }
+
+        public override void OnEnter()
+        {
+            _duration = StateMachine.IdleDuration;
+        }
+
+        public override void OnExit()
+        {
+        }
+
+        public override void OnUpdate()
+        {
+            _time += Time.deltaTime;
+            if (_time >= _duration)
+                StateMachine.SwitchState(new EnemyWalkerWalkState(Composition, StateMachine));
+        }
+
+        public override void OnFixedUpdate()
+        {
+        }
+    }
+}
