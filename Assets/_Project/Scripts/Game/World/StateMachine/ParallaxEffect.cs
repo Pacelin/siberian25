1e9ff8b0ed73d5950477f952ed7e533eb7cb4436
Pacelin <Warfacer2020@gmail.com>
2025-05-04 15:32:05 +0300

    feat: Parallax

---
+++ Assets/_Project/Scripts/Game/World/StateMachine/ParallaxEffect.cs
@@ -0,0 +1,24 @@
+﻿using TSS.Core;
+using UnityEngine;
+
+namespace Siberian25.Game.World
+{
+    public class ParallaxEffect : MonoBehaviour
+    {
+        [SerializeField] private float _xMultiplier = 0.2f;
+        [SerializeField] private float _yMultiplier = 0.2f;
+
+        private Vector2 _lastCameraPos;
+        private void Awake() => _lastCameraPos = SceneCameraProvider.MainCamera.transform.position;
+
+        private void Update()
+        {
+            var curPos = (Vector2) SceneCameraProvider.MainCamera.transform.position;
+            var delta = curPos - _lastCameraPos;
+            var myDelta = new Vector2(delta.x * _xMultiplier, delta.y * _yMultiplier);
+
+            transform.position += (Vector3) myDelta;
+            _lastCameraPos = curPos;
+        }
+    }
+}
